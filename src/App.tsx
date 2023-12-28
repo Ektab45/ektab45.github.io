27011f7ca79b81447dfcf7c3a79647422db890c9
Ektab45 <ekta.budhaprithi@gmail.com>
2023-12-28 07:09:24 +0545

    Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello, I am Ekta.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
